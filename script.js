979a955ac067a9e657f6468b3ee97954c773cb2c
Dustin Wurtz <dtwurtz@gmail.com>
2016-03-08 12:25:39 -0800

    Formated conditions dropdowns

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-var fillConditionBox = function() {
-  
-}
+macularSection.addEventListener('click', function(){
+  selectedCondition.innerHTML = 'Macular Degeneration, AMD';
+})
+
+alzheimerSection.addEventListener('click', function() {
+  selectedCondition.innerHTML = 'Alzheimer Disease';
+})
+
+coronarySection.addEventListener('click', function() {
+  selectedCondition.innerHTML = 'Coronary Artery Disease';
+})
