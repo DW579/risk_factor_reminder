89fe74ea0081a3c8d221f86b0dcd1a185c0ae557
Dustin Wurtz <dtwurtz@gmail.com>
2016-03-09 10:03:32 -0800

    Api is mostly figured out. Changed form layout. Connecting the frist two parts of the form.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Condition section
 macularSection.addEventListener('click', function(){
   selectedCondition.innerHTML = 'Macular Degeneration, AMD';
 })
@@ -9,3 +10,85 @@ alzheimerSection.addEventListener('click', function() {
 coronarySection.addEventListener('click', function() {
   selectedCondition.innerHTML = 'Coronary Artery Disease';
 })
+
+// Submit button event listener
+submitButton.addEventListener('click', function() {
+  var finishedForm = {
+    yearOfBirth: birthYear.value,
+    gender: gender.value,
+    ethnicity: eth.value,
+    'weight(lbs)': weightInput.value,
+    'height(in)': heightInput.value,
+  };
+  console.log(finishedForm);
+  var stringFinishedForm = (JSON.stringify(finishedForm));
+  console.log(stringFinishedForm);
+
+ if(selectedCondition.innerHTML === 'Macular Degeneration, AMD') {
+   var settings = {
+     "async": true,
+     "crossDomain": true,
+     "url": "https://api.basehealth.com/Genophen/api/v1/assessment/diseases/ageRelatedMacularDegeneration",
+     "method": "POST",
+     "headers": {
+       "user_key": "ded92f1e09ae6a59c97207b6bb0194d2",
+       "content-type": "application/json",
+       "cache-control": "no-cache"
+     },
+     "processData": false,
+    //  "data": "{  \"gender\": \"hello\", \"yearOfBirth\": \"goodbye\", \"ethnicity\": \"please\"\n}"
+     data: JSON.stringify({  "gender": gender.value, "yearOfBirth": birthYear.value, "ethnicity": eth.value}),
+   }
+
+   $.ajax(settings).done(function (response) {
+     alert(response);
+   });
+  //  $.ajax({
+  //    url: 'https://api.basehealth.com/Genophen/api/v1/assessment/diseases/alzheimersDisease',
+  //    headers: {
+  //      "user_key" : "ded92f1e09ae6a59c97207b6bb0194d2"
+  //    },
+  //    contentType: 'application/json',
+  //    data: {
+  //       gender : "male",
+  //       yearOfBirth : "1965"
+  //     },
+  //    dataType: "JSON",
+  //    method: 'POST',
+  //    success: function(data) {
+  //      alert(JSON.stringify(data));
+  //    },
+  //    error: function (data) {
+  //      console.log('error: ', data);
+  //    }
+  //  })
+ } else if(selectedCondition.innerHTML === 'Alzheimer Disease') {
+  console.log(JSON.stringify(finishedForm));
+ } else {
+   console.log('you are on a role');
+ }
+})
+
+
+// var finishedForm = {
+//   yearOfBirth: birthYear.value,
+//   gender: gender.value,
+//   ethnicity: eth.value,
+//   'weight(lbs)': weightInput.value,
+//   'height(in)': heightInput.value,
+// };
+//
+// var settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "https://api.basehealth.com/Genophen/api/v1/assessment/diseases/alzheimersDisease",
+//   "method": "POST",
+//   "headers": {
+//     "user_key": "ded92f1e09ae6a59c97207b6bb0194d2",
+//     "content-type": "application/json",
+//     "cache-control": "no-cache"
+//   },
+//   "processData": false,
+//  //  "data": "{  \"gender\": \"gender.value\", \"yearOfBirth\": \"birthYear.value\", \"ethnicity\": \"eth.value\"\n}"
+//  finishedForm,
+// }
